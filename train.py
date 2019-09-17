f3d59c2b82be3c2fa34e92283893c0108ddaae73
Taro <taromakino@gmail.com>
2019-09-17 09:25:11 -0400

    Single support and query sets

--- train.py
+++ train.py
@@ -97,17 +97,11 @@ class MLDGTrainer(MLPTrainer):
         self.net.train()
         for itr in range(self.config['num_itrs']):
             self.itr = itr
-            support_fold = np.random.choice(a=np.arange(0, len(self.data_train)), size=1)[0]
-            data_support = self.data_train[support_fold]
-            x, y = data_support.get_batch()
-            support_loss = F.mse_loss(self.net(x), y)
-            x_query, y_query = [], []
-            for query_fold in range(len(self.data_train)):
-                if query_fold != support_fold:
-                    x, y = self.data_train[query_fold].get_batch()
-                    x_query.append(x)
-                    y_query.append(y)
-            x_query, y_query = torch.cat(x_query), torch.cat(y_query)
+            support_fold, query_fold = np.random.choice(self.config['num_folds'], 2, replace=False)
+            data_support, data_query = self.data_train[support_fold], self.data_train[query_fold]
+            x_support, y_support = data_support.get_batch()
+            support_loss = F.mse_loss(self.net(x_support), y_support)
+            x_query, y_query = data_query.get_batch()
             y_hat = self.net(
                 x=x_query,
                 meta_loss=support_loss,
